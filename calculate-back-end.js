d0cce90de6a45fc00f6a6c53e4d1e4a1d5fde395
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-06-22 22:13:09 -0400

    passed all tests

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -2,7 +2,21 @@
  * YOUR CODE HERE *
  ******************/
 
-
+function calculate(x, y, z){
+  x = Number(x);
+  y = Number(y);
+  if(z === '+' || z === 'plus' || z === 'added to'){
+    return x + y
+  }else if(z === '-' || z === 'minus' || z === 'subtracted from'){
+    return x - y
+  }else if(z === 'x' || z === 'X' || z === 'times' || z === 'multiplied by'){
+    return y * x
+  }else if(z === "divided by" || z === '/'){
+    return x / y
+  }else if(z === '%' || z === 'mod' || z === 'modulus'){
+    return x % y
+  }else return 'Sorry, that\'s not a mathematical operation!'
+}
  
 
 /********************************************
